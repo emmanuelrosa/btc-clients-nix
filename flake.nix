4f6d07cae877ef58f0fbc9e731c99800ddb80859
Emmanuel Rosa <emmanuelrosa@protonmail.com>
2026-07-03 09:53:03 -0400

    refactor flake.nix

--- flake.nix
+++ flake.nix
@@ -5,17 +5,29 @@
     nixpkgs.url = "github:nixos/nixpkgs";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs }: let
+    lib = nixpkgs.lib;
 
+    # A function to build the `packages` attribute.
+    # If available, pkg.meta.platforms is checked for compatibility.
+    forAllSystems = func: lib.genAttrs [
+      "x86_64-linux"
+     "aarch64-linux"
+    ] (system:
+      lib.filterAttrs
+        (pname: pkg: 
+          (!(builtins.hasAttr "meta" pkg))
+          || (!(builtins.hasAttr "platforms" pkg.meta))
+          || (builtins.elem system pkg.meta.platforms))
+        (func (import nixpkgs { inherit system; }) system)
+    );
+  in {
     nixosModules.datum_gateway = import ./modules/datum_gateway/module.nix;
     nixosModules.hashgg = import ./modules/hashgg/module.nix;
 
-    packages.x86_64-linux = let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages."${system}";
-    in {
+    packages = forAllSystems (pkgs: system: rec {
       bisq = pkgs.callPackage ./pkgs/bisq {};
-      bisq-desktop = self.packages."${system}".bisq;
+      bisq-desktop = bisq;
       bisq2 = pkgs.callPackage ./pkgs/bisq2 {};
       sparrow = pkgs.callPackage ./pkgs/sparrow {};
       sparrow-get-source-hashes = pkgs.callPackage ./pkgs/sparrow/get-source-hashes.nix {};
@@ -25,11 +37,11 @@
       playit = pkgs.callPackage ./pkgs/playit {};
       playit-get-source-hashes = pkgs.callPackage ./pkgs/playit/get-source-hashes.nix {};
       hashgg = pkgs.callPackage ./pkgs/hashgg { 
-        inherit (self.packages.x86_64-linux) playit;
+        inherit (self.packages."${system}") playit;
       };
 
       update-checker = pkgs.callPackage ./pkgs/update-checker {
-        inherit (self.packages.x86_64-linux)
+        inherit (self.packages."${system}")
           bisq
           bisq2
           sparrow
@@ -39,19 +51,6 @@
           playit
           hashgg;
       };
-    };
-
-    packages.aarch64-linux = let
-      system = "aarch64-linux";
-      pkgs = nixpkgs.legacyPackages."${system}";
-    in {
-      sparrow = pkgs.callPackage ./pkgs/sparrow {};
-      sparrow-get-source-hashes = pkgs.callPackage ./pkgs/sparrow/get-source-hashes.nix {};
-      bisq2 = pkgs.callPackage ./pkgs/bisq2 {};
-      bitcoin-tui = pkgs.callPackage ./pkgs/bitcoin-tui {};
-      rpcauth = pkgs.callPackage ./pkgs/rpcauth {};
-      datum_gateway = pkgs.callPackage ./pkgs/datum_gateway {};
-      playit = pkgs.callPackage ./pkgs/playit {};
-    };
+    });
   };
 }
