52d19441610fc8463c5bbcfd6596fc8c6a866611
Emmanuel Rosa <emmanuelrosa@protonmail.com>
2026-09-08 08:42:01 -0400

    Revert "deprecate hashgg and playit"
    This reverts commit dbcc3ddff3a08a8018cdd78c58c82e7ede7d26c0.

--- flake.nix
+++ flake.nix
@@ -34,9 +34,9 @@
       bitcoin-tui = pkgs.callPackage ./pkgs/bitcoin-tui {};
       rpcauth = pkgs.callPackage ./pkgs/rpcauth {};
       datum_gateway = pkgs.callPackage ./pkgs/datum_gateway {};
-      playit = lib.warn "playit will be removed from btc-clients-nix" pkgs.callPackage ./pkgs/playit {};
+      playit = pkgs.callPackage ./pkgs/playit {};
       playit-get-source-hashes = pkgs.callPackage ./pkgs/playit/get-source-hashes.nix {};
-      hashgg = lib.warn "hashgg will be removed from btc-clients-nix" pkgs.callPackage ./pkgs/hashgg { 
+      hashgg = pkgs.callPackage ./pkgs/hashgg { 
         inherit (self.packages."${system}") playit;
       };
 
--- modules/hashgg/module.nix
+++ modules/hashgg/module.nix
@@ -108,10 +108,6 @@ in {
         (instance: { name = "hashgg-${instance}"; value = instances."${instance}"; }) (builtins.attrNames instances)
       );
   in lib.mkIf cfg.enable {
-    warnings = [
-      "services.hashgg is deprecated and will be removed from btc-clients-nix."
-    ];
-
     assertions = [
       {
         assertion = dgCfg.enable;
